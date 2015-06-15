92338729768df45228edae9b5c81bd6bd13b5a57
Vadim Korkin <xaltotun.great@gmail.com>
2015-06-15 18:31:00 +0400

    Minor fixes in regex config

--- REAL0/REAL_Plugin1/src/org/eclipselabs/real/gui/e4swt/parts/GUISearchResult.java
+++ REAL0/REAL_Plugin1/src/org/eclipselabs/real/gui/e4swt/parts/GUISearchResult.java
@@ -470,8 +470,8 @@ public class GUISearchResult {
             final Integer sortColIndex = newSortColIndex;
             if (sortColIndex > 0) {
                 log.debug("sortColIndex " + sortColIndex + " sortDirection " + sortDirection);
-                /* using parallelStream for sorting doesn't name sense
-                 * because sort running time is small compared to disposing/creating table items.
+                /* using parallelStream for sorting doesn't make sense
+                 * because the running time of sorting is small compared to disposing/creating table items.
                  */
                 Collections.sort(rows, new Comparator<IDRViewItem>() {
 
